527e68ba6087d59748ad8fc1b0a1cc263f1b3166
Amy Palamountain <amy@palamounta.in>
2013-09-20 21:17:12 +1200

    link client and resource client
    as seperate console projects

---
+++ HyperLibrary.LinkClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.Http.SelfHost;
+using HyperLibrary.Core;
+
+namespace HyperLibrary.LinkClient
+{
+    class Program
+    {
+        private static readonly Uri ServerUri = new Uri("http://localhost:9200");
+   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*************** Starting the Hyper Library *****************");
+
+            var config = new HttpSelfHostConfiguration(ServerUri);
+
+            var apiServiceConfiguration = new ApiServiceConfiguration(config);
+            apiServiceConfiguration.Configure();
+
+            var host = new HttpSelfHostServer(config);
+
+            host.OpenAsync().Wait();
+            LibraryLinkExplorer d = new LibraryLinkExplorer(new Uri(ServerUri, "api"));
+            d.Explore().Wait();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+
+            host.CloseAsync().Wait();
+        }
+
+     
+    }
+}
---
+++ HyperLibrary.ResouceClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Http.SelfHost;
+using HyperLibrary.Core;
+
+namespace HyperLibrary.ResouceClient
+{
+    class Program
+    {
+        private static readonly Uri ServerUri = new Uri("http://localhost:9200");
+   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*************** Starting the Hyper Library *****************");
+
+            var config = new HttpSelfHostConfiguration(ServerUri);
+
+            var apiServiceConfiguration = new ApiServiceConfiguration(config);
+            apiServiceConfiguration.Configure();
+
+            var host = new HttpSelfHostServer(config);
+
+            host.OpenAsync().Wait();
+
+            var d = new LibraryExplorer(ServerUri);
+            d.Explore().Wait();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+
+            host.CloseAsync().Wait();
+        }
+    }
+}
